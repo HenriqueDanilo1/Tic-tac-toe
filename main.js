a6fbb812ea302918f151998cb94a0702b9b5c841
HenriqueDanilo1 <danilohenolive@gmail.com>
2024-07-02 10:09:09 -0300

    a

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let turn = 1
+let dummy = 0
 let painel = document.getElementById('painel')
+let table = document.getElementById('table')
+let res = document.getElementById('res')
+let td = document.getElementsByTagName('td')
+
 let a = document.getElementById('1')
 let b = document.getElementById('2')
 let c = document.getElementById('3')
@@ -9,7 +14,7 @@ let f = document.getElementById('6')
 let g = document.getElementById('7')
 let h = document.getElementById('8')
 let i = document.getElementById('9')
-let table = document.getElementById('table')
+
 
 function play(me){
     if(turn == 1){
@@ -17,23 +22,47 @@ function play(me){
         turn+=1
         painel.innerHTML="Its O turn"
         me.classList.add("X")
+        dummy+=1
     } else{
         me.innerHTML="O"
         turn-=1
         painel.innerHTML="Its X turn"
         me.classList.add("O")
+        dummy+=1
     }
     me.removeAttribute('onclick')
     if(
-        ((a, b, c).classList.contains("O")) &&
-        ((d, e, f).classList.contains("O")) &&
-        ((g, h, i).classList.contains("O")) &&
-        ((a, f, i).classList.contains("O")) &&
-        ((c, e, g).classList.contains("O"))
+        (a.classList.contains("O") && b.classList.contains("O") && c.classList.contains("O")) ||
+        (d.classList.contains("O") && e.classList.contains("O") && f.classList.contains("O")) ||
+        (g.classList.contains("O") && h.classList.contains("O") && i.classList.contains("O")) ||
+        (a.classList.contains("O") && e.classList.contains("O") && i.classList.contains("O")) ||
+        (c.classList.contains("O") && e.classList.contains("O") && g.classList.contains("O"))
     ){gg("O")}
+    if(
+        (a.classList.contains("X") && b.classList.contains("X") && c.classList.contains("X")) ||
+        (d.classList.contains("X") && e.classList.contains("X") && f.classList.contains("X")) ||
+        (g.classList.contains("X") && h.classList.contains("X") && i.classList.contains("X")) ||
+        (a.classList.contains("X") && e.classList.contains("X") && i.classList.contains("X")) ||
+        (c.classList.contains("X") && e.classList.contains("X") && g.classList.contains("X"))
+    ){gg("X")}
+    if(dummy==9){notgg()}
 }
 
 function gg(team){
     table.style.pointerEvents='none'
-    painel.innerHTML=`${team} win!`
+    painel.innerHTML=`${team} won!`
+    painel.classList.add(`${team}`)
+    res.classList.remove('invisible')
+    dummy = 0
+}
+
+function notgg(){
+    table.style.pointerEvents='none'
+    painel.innerHTML=`No one won!`
+    res.classList.remove('invisible')
+    dummy = 0
+}
+
+function restart(){
+    td.classList.remove('X')
 }
